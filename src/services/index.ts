42372808beb27c1feea49d62596a9563614cef5c
Zeinab Rezaee <zeinab.rezaee67@gmail.com>
2022-09-30 18:59:51 +0330

    fix: Remove log and fix formatting

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,13 +2,12 @@ export function getRequestAPI<T>(url: string): Promise<T> {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then(response => {
-        console.log({response});
         if (!response.ok) {
-          throw new Error(response.statusText)
+          throw new Error(response.statusText);
         }
-        
-        resolve(response.json())})
-      .catch(reject)
-  })
-  
+
+        resolve(response.json());
+      })
+      .catch(reject);
+  });
 }
